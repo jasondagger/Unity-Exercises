7e4aa2c2eac7060b0934ac380f60dd4274d63a6d
jasondagger <jasondagger1993@gmail.com>
2025-05-28 23:16:20 -0700

    initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,198 @@
+
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Transform[] GroundRaycasts                  = null;
+    [SerializeField] private float       MovementSpeed                   = 3f;
+    [SerializeField] private float       RotationSpeedInDegreesPerSecond = 135f;
+    [SerializeField] private float       InitialJumpVelocity             = 100f;
+    [SerializeField] private int         NumberOfJumps                   = 2;
+    
+    private int       m_numberOfJumpsRemaining = 2;
+    private bool      m_isGrounded             = true;
+    private bool      m_isAscending            = false;
+    private bool      m_isDescending           = false;
+    private float     m_jumpVelocity           = 0f;
+    
+    private void FixedUpdate()
+    {
+        this.HandleJumpAscending();
+        this.HandleJumpDescending();
+    }
+    
+    private void Update()
+    {
+        this.HandleGroundDetection();
+        this.HandleJump();
+        this.HandleRotation();
+        this.HandleMovement();
+    }
+
+    private bool CanJump()
+    {
+        return m_numberOfJumpsRemaining > 0;
+    }
+
+    private void HandleGroundDetection()
+    {
+        this.m_isGrounded = false;
+        foreach (var groundRaycast in this.GroundRaycasts)
+        {
+            var hit = Physics.Raycast(
+                origin:      groundRaycast.position, 
+                direction:   Vector3.down,
+                hitInfo:     out var hitInfo,
+                maxDistance: 0.15f
+            );
+            if (
+                hit is false || 
+                hitInfo.collider.gameObject.layer != LayerMask.NameToLayer("Ground")
+            )
+            {
+                continue;
+            }
+            
+            this.m_isGrounded = true;
+            this.m_numberOfJumpsRemaining = this.NumberOfJumps;
+            this.m_jumpVelocity = 0f;
+            this.m_isDescending = false;
+            break;
+        }
+    }
+
+    private void HandleJump()
+    {
+        if (this.CanJump() is false)
+        {
+            return;
+        }
+        
+        var isPressedJump = Input.GetKeyDown(
+            key: KeyCode.Space
+        );
+        if (isPressedJump is false)
+        {
+            return;
+        }
+        
+        this.m_numberOfJumpsRemaining--;
+        this.m_isAscending = true;
+        this.m_jumpVelocity = this.InitialJumpVelocity;
+    }
+
+    private void HandleJumpAscending()
+    {
+        if (this.m_isAscending is false)
+        {
+            return;
+        }
+        
+        this.transform.position += this.m_jumpVelocity * Time.deltaTime * Vector3.up;
+        this.m_jumpVelocity += Physics.gravity.y * Time.deltaTime;
+
+        if (this.m_jumpVelocity > 0)
+        {
+            return;
+        }
+        
+        this.m_isAscending = false;
+        this.m_isDescending = true;
+    }
+
+    private void HandleJumpDescending()
+    {
+        if (this.m_isDescending is false)
+        {
+            return;
+        }
+        
+        this.transform.position += this.m_jumpVelocity * Time.deltaTime * Vector3.up;
+        this.m_jumpVelocity += Physics.gravity.y * Time.deltaTime;
+    }
+    
+    private void HandleMovement()
+    {
+        var velocity = Vector3.zero;
+
+        var isPressingForward = Input.GetKey(
+            key: KeyCode.W
+        );
+        var isPressingBackward = Input.GetKey(
+            key: KeyCode.S
+        );
+        var isPressingLeftward = Input.GetKey(
+            key: KeyCode.A
+        );
+        var isPressingRightward = Input.GetKey(
+            key: KeyCode.D
+        );
+        
+        if (isPressingForward is true)
+        {
+            velocity.z += Time.deltaTime * this.MovementSpeed;
+        }
+        if (isPressingBackward is true)
+        {
+            velocity.z -= Time.deltaTime * this.MovementSpeed;
+        }
+        if (isPressingLeftward is true)
+        {
+            velocity.x -= Time.deltaTime * this.MovementSpeed;
+        }
+        if (isPressingRightward is true)
+        {
+            velocity.x += Time.deltaTime * this.MovementSpeed;
+        }
+        
+        velocity = this.transform.TransformDirection(
+            direction: velocity
+        );
+
+        this.transform.position += velocity;
+    }
+
+    private void HandleRotation()
+    {
+        var eulerAngles = this.transform.eulerAngles;
+
+        var isPressedTurnLeft = Input.GetKey(
+            key: KeyCode.Q
+        );
+        var isPressedTurnRight = Input.GetKey(
+            key: KeyCode.E
+        );
+        
+        if (isPressedTurnLeft is true)
+        {
+            eulerAngles.y += this.RotationSpeedInDegreesPerSecond * Time.deltaTime;
+        }
+        if (isPressedTurnRight is true)
+        {
+            eulerAngles.y -= this.RotationSpeedInDegreesPerSecond * Time.deltaTime;
+        }
+        
+        this.transform.eulerAngles = eulerAngles;
+    }
+
+    private bool IsGrounded()
+    {
+        return this.m_isGrounded;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
